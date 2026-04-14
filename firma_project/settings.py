6d583b356a8f0ffe7a096e6b7a30367504c4cf5f
Camilo calderon <cacalderon@gane.com.co>
2026-04-14 11:05:00 -0500

    bug fix collectstatic

--- firma_project/settings.py
+++ firma_project/settings.py
@@ -220,7 +220,7 @@ STORAGES = {
         },
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
